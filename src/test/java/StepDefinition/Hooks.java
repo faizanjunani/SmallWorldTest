95811f823e6f68f7500f7525573df885d67382b7
faizanjunani <faizanjunani@yahoo.com>
2023-05-03 17:52:12 +0500

    changes committed

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -1,22 +1,20 @@
 package StepDefinition;
 
 import common.Base;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
+import io.cucumber.java.BeforeAll;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hooks extends Base {
-    @Before //Cucumber Before Hook
-    public void setupDriver() throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver","C:/Users/FaizanJunani/Downloads/chromedriver_win32/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        //  driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+    @BeforeAll//Cucumber Before Hook
+    public static void setupDriver() throws InterruptedException {
+        InitializeDriver();
     }
 
-    @After // Cucumber After hook
-    public void quitDriver() throws Exception {
-        //driver.quit();
+    @AfterAll// Cucumber After hook
+    public static void quitDriver() throws Exception {
+        driver.quit();
     }
 }
--- src/test/java/common/Base.java
+++ src/test/java/common/Base.java
@@ -1,7 +1,18 @@
 package common;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
-    public static WebDriver driver;
+    public static WebDriver driver=null;
+
+    public static void InitializeDriver(){
+        if(driver== null){
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        else{
+            return;
+        }
+    }
 }
